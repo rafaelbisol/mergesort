a581b1742c75f03f6a816a91ff7d65c6561eb5d7
Rafael Bisol <rafaelbisol@outlook.com>
2018-06-08 15:07:26 -0300

    mudou essa porra

--- mergesort.c
+++ mergesort.c
@@ -1,1 +1,15 @@
+void MergeSort (int vetor[], int vetoraux[], int esquerda, int direita)
+{
+    if (esquerda < direita)
+    {
+        int m = (esquerda + direita) / 2;
+        MergeSort(vetor, vetoraux, esquerda, m);
+        MergeSort(vetor, vetoraux, m + 1, direita);
+        Intercala(vetor, vetoraux, esquerda, m, direita);
+    }
+}
 
+void Intercala (int vetor[], int vetoraux[], int esquerda, int m, int direita)
+{
+
+}
